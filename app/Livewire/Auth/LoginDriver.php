a2fdb2da129d51eb0ba4328672ad71e7e137590b
arttema <arttema@mail.ru>
2024-12-18 00:17:28 +0300

    feat: added autentification

--- app/Livewire/Auth/LoginDriver.php
+++ app/Livewire/Auth/LoginDriver.php
@@ -3,11 +3,35 @@
 namespace App\Livewire\Auth;
 
 use Livewire\Component;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginDriver extends Component
 {
+    public $email;
+    public $password;
+
     public function render()
     {
         return view('livewire.auth.login-driver');
     }
+
+    public function authenticate(Request $request) //: RedirectResponse
+    {
+        $credentials = $this->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        //dd($credentials);
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+
 }
